44630c102eb069fb24c1d5fd9c5f285f64f38588
zpf2012 <pengfei.zhang@hand-china.com>
2018-09-19 19:48:18 +0800

    Init Project

---
+++ src/main/java/com/hand/tencent/ocr/imageprocess/ImageProcessApplication.java
@@ -0,0 +1,12 @@
+package com.hand.tencent.ocr.imageprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessApplication.class, args);
+	}
+}
